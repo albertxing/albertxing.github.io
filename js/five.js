4b4f4f1fb55416d78dedcaffffcb955007bf056a
Albert Xing <albert.xing@alumni.ubc.ca>
2013-04-27 21:58:29 -0700

    Clean up JS, add title

--- js/five.js
+++ js/five.js
@@ -1,21 +1,22 @@
-/*jslint browser: true*/
+/*jslint browser: true, vars: true*/
+/*global $*/
 
 // Global variabls
 var textCanvas  = document.getElementById("body"),
-suggestions = ["work", "portfolio", "projects", "about", "info", "contact", "email"],
-suggested   = "",
-currSuggest = "",
-current     = "",
-i           = 0,
-mLeft       = 10,
-mTop        = 20,
-rx          = 0,
-ry          = mTop - 13,
-tab         = 0,
-line        = 0,
-lineHeight  = 18,
-ready       = false,
-five        = true;
+    suggestions = ["work", "portfolio", "projects", "about", "info", "contact", "email"],
+    suggested   = "",
+    currSuggest = "",
+    current     = "",
+    i           = 0,
+    mLeft       = 10,
+    mTop        = 20,
+    rx          = 0,
+    ry          = mTop - 13,
+    tab         = 0,
+    line        = 0,
+    lineHeight  = 18,
+    ready       = false,
+    five        = true;
 
 // Temporary canvas variables
 var ctx, blinkCanvas, bctx, suggCanvas, sctx;
@@ -23,18 +24,13 @@ var ctx, blinkCanvas, bctx, suggCanvas, sctx;
 // Keyword variables
 var initial, work, portfolio, projects, about, info, contact, email;
 
-// AJAX requests
-$.get('work.html', function(data) {
-    array = data.split("\n");
-    work = portfolio = projects = new Code(array, runWork);
-});
-
 /**
  * The code to output and the callback associated with it
  * @param {Array}   input    The code to output, one line per array item
  * @param {Function} callback The callback function to call after code is written
  */
-var Code = function(input, callback) {
+var Code = function (input, callback) {
+    
     this.code = input;
     this.callback = callback;
 };
@@ -48,37 +44,34 @@ contact = email = new Code(["<div id='contact'>CONTACT</div>", "<style>", "\t#co
 var printInt, blinkAnim, blinkInt, cWidth, cHeight, ouput, charWidth, allWidth, winCharLength, numLines;
 
 function parameters() {
-
-    "use strict";
-
+    
     cWidth = textCanvas.width = blinkCanvas.width = suggCanvas.width = window.innerWidth;
     cHeight = textCanvas.height = blinkCanvas.height = suggCanvas.height = window.innerHeight;
     ctx.font = sctx.font = "1em 'DejaVu Sans Mono', Monaco, Consolas, Ubuntu Mono, monospace";
     sctx.fillStyle = "#478EAB";
     ctx.fillStyle = "#AAA";
     bctx.fillStyle = "#CCC";
-
+    
     var all = "";
+    
     for (var l in output) {
         all += output[l];
     }
-
+    
     charWidth = ctx.measureText(" ").width;
     allWidth = ctx.measureText(all).width;
     winCharLength = Math.floor((cWidth - 2 * mLeft) / charWidth);
-
+    
     numLines = Math.floor(window.innerHeight / lineHeight);
 }
 
 function reorder() {
-
-    "use strict";
-
+    
     var all = "";
     for (var l in output) {
         all += output[l];
     }
-
+    
     line = 0;
     output = [all];
     while (output[line].length >= winCharLength) {
@@ -253,12 +246,12 @@ function detKey(e) {
     else {
         // If there are witches, there must be vampires
         var keyID = e.which;
-
+        
         // Ghosts, come forward.
         clearInterval(blinkInt);
         clearInterval(blinkAnim);
         bctx.globalAlpha = 1;
-
+        
         // To be or not to be
         if (output[line].substr(output[line].length - 7,output[line].length) == dF("Rfwnxxf5")) {
             document.querySelector("body").style.background = dF("dsjntpo1");
@@ -266,10 +259,10 @@ function detKey(e) {
             bctx.fillStyle = "#DDD";
             render(output);
         }
-
+        
         blink();
         blinkInt = setInterval(blink, 1200);
-
+        
         // Too lazy to change the function name?
         return retKey(keyID,e.shiftKey,e.ctrlKey);
     }
@@ -279,87 +272,87 @@ function retKey(keyID,shift,ctrl) {
     // Light switch
     switch (true)
     {
-    // Functions
-    case (keyID >= 112 && keyID <= 123):
-    break;
-
-    // Unlucky! (Return)
-    case (keyID == 13):
-    var all = "";
-    for (var l in output) {
-        all += output[l];
+            // Functions
+        case (keyID >= 112 && keyID <= 123):
+            break;
+            
+            // Unlucky! (Return)
+        case (keyID == 13):
+            var all = "";
+            for (var l in output) {
+                all += output[l];
+            }
+            if (suggested.indexOf(output[line].toLowerCase()) !== -1) {
+                line = 0;
+                $("#output").html("").addClass("hidden");
+                go(window[suggested].code, false, true, window[suggested].callback);
+                return;
+            } else if (all.search(/^[^<]*<([A-z][A-z0-9]*).*>.*<\/\1>[^<]*$/) !== -1) {
+                output[line] += mapKey(shift, keyID);
+                line = 0;
+                output = [all];
+                writeCode(output,output[line].length);
+            } else {
+                output[line + 1] = "";
+                rx = 0;
+                line++;
+            }
+            render(output);
+            suggest();
+            break;
+            
+            // I've heard of a holiday called Easter
+        case (keyID == 55 && shift):
+            document.querySelector("body").style.background = "white";
+            document.querySelector("#hold").style.color = "#333";
+            bstyle.color = "#333";
+            ctx.fillStyle = "#333";
+            bctx.fillStyle = "#DDD";
+            render(output);
+            break;
+            
+            // Click - clack
+        case (keyID == 82 && ctrl && shift):
+            window.location.reload(true);
+            break;
+            
+            // Click - clack
+        case ((keyID == 82 && ctrl) || keyID == 116):
+            window.location.reload();
+            break;
+            
+            // Boom
+        case (keyID == 46 || keyID == 8):
+            if (output[line] === "" && line !== 0) {
+                output.splice(line);
+                line--;
+            } else {
+                output[line] = output[line].substring(0,output[line].length-1);
+            }
+            render(output);
+            suggest();
+            return false;
+            
+            // Autocomplete
+        case (keyID == 39 || keyID == 9):
+            output[line] += currSuggest;
+            suggest();
+            render(output);
+            return false;
+            
+        default:
+            output[line] += mapKey(shift, keyID);
+            fold(output[line],output[line].length);
+            render(output);
+            suggest();
+            break;
     }
-    if (suggested.indexOf(output[line].toLowerCase()) !== -1) {
-        line = 0;
-        $("#output").html("").addClass("hidden");
-        go(window[suggested].code, false, true, window[suggested].callback);
-        return;
-    } else if (all.search(/^[^<]*<([A-z][A-z0-9]*).*>.*<\/\1>[^<]*$/) !== -1) {
-        output[line] += mapKey(shift, keyID);
-        line = 0;
-        output = [all];
-        writeCode(output,output[line].length);
-    } else {
-        output[line + 1] = "";
-        rx = 0;
-        line++;
-    }
-    render(output);
-    suggest();
-    break;
-
-    // I've heard of a holiday called Easter
-    case (keyID == 55 && shift):
-    document.querySelector("body").style.background = "white";
-    document.querySelector("#hold").style.color = "#333";
-    bstyle.color = "#333";
-    ctx.fillStyle = "#333";
-    bctx.fillStyle = "#DDD";
-    render(output);
-    break;
-
-    // Click - clack
-    case (keyID == 82 && ctrl && shift):
-    window.location.reload(true);
-    break;
-
-    // Click - clack
-    case ((keyID == 82 && ctrl) || keyID == 116):
-    window.location.reload();
-    break;
-
-    // Boom
-    case (keyID == 46 || keyID == 8):
-    if (output[line] === "" && line !== 0) {
-        output.splice(line);
-        line--;
-    } else {
-        output[line] = output[line].substring(0,output[line].length-1);
-    }
-    render(output);
-    suggest();
-    return false;
-
-    // Autocomplete
-    case (keyID == 39 || keyID == 9):
-    output[line] += currSuggest;
-    suggest();
-    render(output);
-    return false;
-
-    default:
-    output[line] += mapKey(shift, keyID);
-    fold(output[line],output[line].length);
-    render(output);
-    suggest();
-    break;
-}
 }
 
 function mapKey(isShiftKey, cCode) {
-
+    
     var character = "";
-
+    
     var characterMap = {
         192: "~",
         49: "!",
@@ -371,62 +364,62 @@ function mapKey(isShiftKey, cCode) {
         55: "&",
         56: "*",
         57: "(",
-            48: ")",
-189: "_",
-187: "+",
-219: "{",
-221: "}",
-220: "|",
-186: ":",
-222: "\"",
-188: "<",
-190: ">",
-191: "?",
-32: " "
-};
-
-var unshift = {
-    192: "`",
-    189: "-",
-    187: "=",
-    219: "[",
-    221: "]",
-    220: "\\",
-    186: ";",
-    222: "'",
-    188: ",",
-    190: ".",
-    191: "/",
-    32: " "
-};
-
-if (cCode >= 0 && cCode <= 46 && cCode !== 32 || cCode === 91 || cCode === 92) {
-    character = '';
-} else if (isShiftKey) {
-
-    if ( cCode >= 65 && cCode <= 90 ) {
-        character = String.fromCharCode(cCode);
-    } else if (characterMap[cCode] !== "undefined") {
-        character = characterMap[cCode];
+        48: ")",
+        189: "_",
+        187: "+",
+        219: "{",
+        221: "}",
+        220: "|",
+        186: ":",
+        222: "\"",
+        188: "<",
+        190: ">",
+        191: "?",
+        32: " "
+    };
+    
+    var unshift = {
+        192: "`",
+        189: "-",
+        187: "=",
+        219: "[",
+        221: "]",
+        220: "\\",
+        186: ";",
+        222: "'",
+        188: ",",
+        190: ".",
+        191: "/",
+        32: " "
+    };
+    
+    if (cCode >= 0 && cCode <= 46 && cCode !== 32 || cCode === 91 || cCode === 92) {
+        character = '';
+    } else if (isShiftKey) {
+        
+        if ( cCode >= 65 && cCode <= 90 ) {
+            character = String.fromCharCode(cCode);
+        } else if (characterMap[cCode] !== "undefined") {
+            character = characterMap[cCode];
+        } else {
+            character = '';
+        }
+        
     } else {
-        character = '';
+        
+        if ( cCode >= 65 && cCode <= 90 ) {
+            character = String.fromCharCode(cCode).toLowerCase();
+        } else if ( cCode >= 48 && cCode <= 57) {
+            character = String.fromCharCode(cCode);
+        } else if (unshift[cCode] !== "undefined") {
+            character = unshift[cCode];
+        }
+            else {
+                character = '';
+            }
     }
-
-} else {
-
-    if ( cCode >= 65 && cCode <= 90 ) {
-        character = String.fromCharCode(cCode).toLowerCase();
-    } else if ( cCode >= 48 && cCode <= 57) {
-        character = String.fromCharCode(cCode);
-    } else if (unshift[cCode] !== "undefined") {
-        character = unshift[cCode];
-    }
-    else {
-        character = '';
-    }
-}
-
-return character;
+    
+    return character;
 }
 
 function suggest() {
@@ -455,34 +448,34 @@ eval(unescape("%66%75%6E%63%74%69%6F%6E%20%64%46%28%73%29%7B%0A%76%61%72%20%73%3
 // Give it a go
 window.onload = function () {
     try {
-
+        
         ctx = textCanvas.getContext("2d");
         blinkCanvas = document.getElementById("blink");
         bctx = blinkCanvas.getContext("2d");
         suggCanvas = document.getElementById("sugg");
         sctx = suggCanvas.getContext("2d");
-
+        
         go(initial);
         var css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = "body { background: #222530; color: #888; padding-left: 0.5ex; }";
         document.getElementsByTagName("head")[0].appendChild(css);
-
+        
         window.onresize = function() {
             parameters();
             reorder();
             render(output);
         };
-
+        
         // Key presses?
         window.onkeydown = function(e) {
             // Sherlock Holmes
             return detKey(e);
-
+            
             // Watson
             // return false;
         };
-
+        
         // Focus, now.
         window.onfocus = function() {
             if (ready) {
@@ -493,18 +486,27 @@ window.onload = function () {
             }
             suggest();
         };
-
+        
         window.onblur = function() {
             bctx.globalAlpha = 0;
             bctx.clearRect(0, 0, cWidth, cHeight);
             clearInterval(blinkInt);
             clearInterval(blinkAnim);
         };
-
+        
         $("html").addClass("show");
-
+        
     } catch (err) {
         console.log(err);
         $("#alt").css("display", "block");
     }
 };
+
+// AJAX requests
+$.get('work.html', function (data) {
+    
+    "use strict";
+    
+    var array = data.split("\n");
+    work = portfolio = projects = new Code(array, runWork);
+});
