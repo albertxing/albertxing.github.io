8f2da98272c2ac2a7e0da6c69d477690e4cdf4fc
Albert Xing <albert.xing@alumni.ubc.ca>
2013-07-21 18:30:44 -0700

    Log lines to draw

--- js/net.js
+++ js/net.js
@@ -6,6 +6,7 @@ $.getJSON("http://jsonip.appspot.com?callback=?", function (data) {
 		console.log(data);
 		$.get('ip', function (data) {
 			var _lines = data.split("\n");
+			console.log(_lines);
 			for (i in _lines) {
 				var mat = _lines[i].split('.');
 				addPoint(mat[0] - 100, mat[1] - 100, mat[2] - 100);
