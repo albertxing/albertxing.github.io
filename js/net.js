731584f38ecc29a3b4bc3ba95fba90b857c92d79
Albert Xing <albert.xing@alumni.ubc.ca>
2013-07-22 16:45:26 -0700

    Add antialias and fix mousemove

--- js/net.js
+++ js/net.js
@@ -1,3 +1,5 @@
+var _loaded = false;
+
 $.post('post/ip.php', function(data) {
 	$.get('ip', function (data) {
 		var _lines = data.split("\n");
@@ -10,37 +12,29 @@ $.post('post/ip.php', function(data) {
 					addPoint(mat[0] - 100, mat[1] - 100, mat[2] - 100, function () {
 						setTimeout(function () {
 							_drawDots(i + 1);
-						}, 10);
+						}, 40);
 					});
 				} else {
 					_drawDots(i + 1);
 				}
 
 			} else {
-				_loaded();
+				_loadedCallback();
 			}
 		})(0);
 	});
 });
 
-function _loaded() {
-	renderer.render(scene, camera);
+function _loadedCallback() {	
 	$("#net").html(renderer.domElement);
-	$('#net > canvas').mousemove(function (e) {
-
-		var t = Math.atan((e.pageX - (window.innerWidth / 2)) / 200),
-		p = -Math.atan((e.pageY - (window.innerHeight / 2)) / 200);
-
-		var x = 200 * Math.sin(t) * Math.cos(p),
-		z = 200 * Math.cos(t) * Math.cos(p),
-		y = 200 * Math.sin(p);
-
-		moveCamera(x, y, z);
-	});
 	$("#knight").addClass('lsd');
+	_loaded = true;
+	pivot(currPos);
 }
 
-var renderer = new THREE.WebGLRenderer();
+var currPos = {pageX: 0, pageY: 0}
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
@@ -52,7 +46,7 @@ var scene = new THREE.Scene();
 scene.fog = new THREE.FogExp2(0xfafafa, 0.004);
 
 function addPoint(x, y, z, callback) {
-	var sphere = new THREE.Mesh(new THREE.SphereGeometry(3, 64, 64), new THREE.MeshBasicMaterial({color: 0xcccccc}));
+	var sphere = new THREE.Mesh(new THREE.SphereGeometry(3, 32, 32), new THREE.MeshBasicMaterial({color: 0xcccccc}));
 	sphere.position.set(x, y, z);
 
 	var lineGeo = new THREE.Geometry();
@@ -69,6 +63,22 @@ function addPoint(x, y, z, callback) {
 	}
 }
 
+function pivot(e) {
+	var t = Math.atan((e.pageX - (window.innerWidth / 2)) / 200),
+	p = -Math.atan((e.pageY - (window.innerHeight / 2)) / 200);
+
+	var x = 200 * Math.sin(t) * Math.cos(p),
+	z = 200 * Math.cos(t) * Math.cos(p),
+	y = 200 * Math.sin(p);
+
+	if (_loaded) {
+		moveCamera(x, y, z);
+	} else {
+		currPos = {pageX: e.pageX, pageY: e.pageY};
+		console.log(currPos);
+	}
+}
+
 function moveCamera(x, y, z) {
 	camera.position.set(x, y, z);
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
@@ -77,8 +87,11 @@ function moveCamera(x, y, z) {
 }
 
 $(window).resize(function () {
+	$("#net").width(window.innerWidth).height(window.innerHeight);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.render(scene, camera);
 });
+
+$("#net").width(window.innerWidth).height(window.innerHeight).mousemove(pivot);
