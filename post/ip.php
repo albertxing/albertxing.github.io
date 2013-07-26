9fbb1ce72d9edf7230383ddc0a4597a0d9fc783e
Albert Xing <albert.xing@alumni.ubc.ca>
2013-07-26 15:13:05 -0700

    Change to windows newline

--- post/ip.php
+++ post/ip.php
@@ -6,7 +6,7 @@ $ip = substr($ip, 0, strrpos($ip, '.'));
 $list = file_get_contents('../ip');
 
 if (strpos($list, $ip) === false) {
-	file_put_contents('../ip', $list . PHP_EOL . $ip);
+	file_put_contents('../ip', $list . "\r\n" . $ip);
 }
 
 ?>
